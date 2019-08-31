2942e20d196489f3d5219778bcd96644e84a3e75
alex90631 <alex90631@cs104u-2a.acad.fullerton.edu>
2019-08-31 14:26:33 -0700

    Jorge Garcia's Restaurant Calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,24 @@
-// Name: 
+// Name: Jorge Garcia
 // This program calculates the tax and tip on a restaurant bill.
+# include <iostream>
+int main()
+{
+  double meal_ cost;
+  double tax;
+  double tip;
+  double total_cost;
+
+  std::cout << "Welcome to Jorge Garcia's Restaurant Bill Calculator!\n";
+  std::cout << "What is the total meal cost?";
+  std::cin>> meal_cost;
+
+  tax = meal_cost * 0.0775;
+  tip = meal_cost * 0.20;
+  total_cost = meal_cost + tax + tip;
+
+  std::cout << "Tax:        $" << tax << std::endl;
+  std::cout << "Tip:        $" << tip << std::endl;
+  std::cout << "Total Bill: $" << total_cost << std::endl;
+
+  return o;
+}
